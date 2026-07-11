7ec77659cd17755a336f619a0f23821a851038bd
Shawon Dey <shawondey188@gmail.com>
2026-07-11 21:57:50 +0600

    problen5.c to calculate and print sums

---
+++ Lab-01/problen5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 55, b = 88, sum = a + b;
+    printf("%d + %d = %d\n", a, b, sum);
+    int c = 55, d = 88;
+    printf("%d + %d = %d", c, d, c + d);
+
+    return 0;
+}
