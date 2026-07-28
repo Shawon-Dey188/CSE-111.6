1661e2e9370b7170af01a588ed48a2796882a87c
Shawon Dey <shawondey188@gmail.com>
2026-07-28 23:02:07 +0600

    Add character classification program in C

---
+++ Lab-03/problem3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') 
+    printf("%c is an uppercase character.\n", ch);
+    else if (ch >= 'a' && ch <= 'z') 
+    printf("%c is a lowercase character.\n", ch);
+    else 
+    printf("%c is not an alphabet.\n", ch);
+    
+
+    return 0;
+}
